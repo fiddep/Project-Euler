c5f94787f2cac2877131f88de71f8f1ae219d735
Fredrik Palmquist <fiddep@telia.com>
2018-04-25 18:08:29 +0200

    problem

---
+++ problem4.js
@@ -0,0 +1,30 @@
+const checkPalindrom = (number1, number2) => {
+  let sum = number1 * number2 + "";
+  let reversed = sum
+    .split("")
+    .reverse()
+    .join("");
+
+  if (sum === reversed) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+let found1 = 0;
+let found2 = 0;
+
+for (let c1 = 999; c1 > 1; c1--) {
+  for (let c2 = 999; c2 > 1; c2--) {
+    if (checkPalindrom(c1, c2)) {
+      if (c1 * c2 > found1 * found2) {
+        found1 = c1;
+        found2 = c2;
+        c2 = 0;
+      }
+    }
+  }
+}
+
+console.log(found1, found2);
